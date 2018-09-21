29f66b47ef39b77412f085f1c8b11db2911af216
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-21 15:55:45 +0000

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -8,7 +8,11 @@ require 'values.rb'
 #  meat: ["chicken", "steak", "salmon"],
 #  grains: ["rice", "pasta"]
 # }
+<<<<<<< HEAD
 def test
+=======
+def test 
+>>>>>>> 4ae83f9797590796b0b6e19d0249a353a4460574
 end
 
 def get_the_min(groceries)
